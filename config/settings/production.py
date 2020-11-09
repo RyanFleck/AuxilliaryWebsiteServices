638d59df938cd5f6f18df53acbb3ae6fbc54aff5
Ryan Fleck <ryan.fleck@protonmail.com>
2020-11-09 12:55:18 -0700

    fix: Restrict CORS origins.

--- config/settings/production.py
+++ config/settings/production.py
@@ -166,5 +166,4 @@ sentry_sdk.init(
 # Your stuff...
 # ------------------------------------------------------------------------------
 CORS_ALLOWED_ORIGINS = ["https://www.ryanfleck.ca", "https://ryanfleck.ca"]
-# TODO: Remove this in the future when security is an issue.
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
