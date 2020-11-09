2f3087602774b48cf7c4fbe4fe687984a0194ec2
Ryan Fleck <Ryan.Fleck@protonmail.com>
2020-11-09 15:36:48 -0700

    fix: Django-ify page URL.

--- services/viewcounts/urls.py
+++ services/viewcounts/urls.py
@@ -3,4 +3,4 @@ from django.urls import path
 from services.viewcounts.views import page_tracking_view
 
 app_name = "viewcounts"
-urlpatterns = [path("page-tracker", page_tracking_view, name="page-tracker")]
+urlpatterns = [path("page-tracker/", page_tracking_view, name="page-tracker")]
